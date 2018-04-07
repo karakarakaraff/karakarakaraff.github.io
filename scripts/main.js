db736b1b7c287c6e6cef2a271639946a3cbfac0e
Kara <kara.a.flaherty@gmail.com>
2018-04-07 11:58:09 -0600

    Tweaked skeleton, added social media icons, added Roboto font

--- scripts/main.js
+++ scripts/main.js
@@ -5,24 +5,37 @@ const titleText = "Website Name";
 const topNavLinks = [['Link1','https://#'],['Link2','https://#'],['Link3','https://#']];
 
 // LOGO
-const logoImg = 'assets/images/test.png';
+const logoImg = 'logo.png';
 
 // SOCIAL MEDIA
-// TODO set up social media icons
+const linkedin = {
+  link: 'https://www.linkedin.com/in/-----',
+  icon: 'fab fa-linkedin'
+};
+const twitter = {
+  link: 'https://twitter.com/-----',
+  icon: 'fab fa-twitter'
+};
+const github = {
+  link: 'https://github.com/-----',
+  icon: 'fab fa-github'
+};
 
 // MAIN NAVIGATION BAR
 const mainNavLinks = [['Link1','https://#'],['Link2','https://#'],['Link3','https://#']];
 
 // FOOTER
 const copyright = {
-  year: new Date().getFullYear(),
-  name: 'Name'
+  name: 'Name',
+  year: new Date().getFullYear()
 };
 
+// FUNCTION TO LOAD AND ASSEMBLE EVERYTHING
 window.onload = function() {
   document.title = titleText;
 
   const topNav = document.getElementById('top-nav');
+  topNav.innerHTML = '';
   for (let link of topNavLinks) {
     topNav.innerHTML += `<a class="nav-link" href="${link[1]}">${link[0]}</a>`;
   }
@@ -31,7 +44,7 @@ window.onload = function() {
   logo.innerHTML = `<a href="index.html"><img class="img-fluid" src="${logoImg}" alt="${copyright.name}"/></a>`;
 
   const socialMedia = document.getElementById('social-media');
-  socialMedia.innerHTML = `social media links go here`;
+  socialMedia.innerHTML = `<a href="${linkedin.link}"><i class="${linkedin.icon} fa-3x" data-fa-transform="shrink-6"></i></a><a href="${twitter.link}"><i class="${twitter.icon} fa-3x" data-fa-transform="shrink-6"></i></a><a href="${github.link}"><i class="${github.icon} fa-3x" data-fa-transform="shrink-6"></i></a>`;
 
   const mainNav = document.getElementById('main-nav');
   mainNav.innerHTML = '';
